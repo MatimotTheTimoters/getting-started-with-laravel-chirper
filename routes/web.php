30905ab92e7e85907cf3215fd014fce80b6f0f47
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-24 14:56:01 +0800

    feat: add login and logout to web routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 
 Route::get('/', [ChirpController::class, 'index']);
 
@@ -20,3 +22,15 @@ Route::view('/register', 'auth.register')
     ->name('register');
 Route::post('/register', Register::class)
     ->middleware('guest');
+
+// Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
